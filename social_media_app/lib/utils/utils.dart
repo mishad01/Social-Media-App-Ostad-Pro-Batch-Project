a4d093537941a336f93f625760e899bde478390b
Mishad <mishadcs2002@gmail.com>
2024-11-29 17:49:55 +0600

    SMAOPB-3: follower and following view demo ui completed

---
+++ social_media_app/lib/utils/utils.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Utils {
+  static AppBar customAppBar(String title) {
+    return AppBar(
+      title: Text(title),
+      backgroundColor: Colors.white,
+    );
+  }
+}
